267ba13ef9fae85a229caa8c8d6a8c29d7b07fa0
n4mr3g <german.n.piccioni@gmail.com>
2023-03-10 15:46:33 +0100

    Functional game, still needs to implement: dealer blackjack logic, split, double, points counter, wins/lose counter, betting, styles

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+// Path to the card images
 const cardsDirectory = "./SVG-cards-1.3/";
 
+// Class "Card" with static method "cardToFilename(value, suit)" and getter for the property "fileName".
+// "get fileName" returns the complete filename (without the path), including extension, of the image of the card that calls it.
 class Card {
-	constructor(value, suit) {
+	constructor(value, suit, isFaceDown = false) {
 		this._value = value;
 		this._suit = suit;
 	}
 
-	static cardToFilename(value, suit) {
-		let name = value;
-		switch (suit) {
+	get value() {
+		return this._value;
+	}
+
+	get suit() {
+		return this._suit;
+	}
+
+	get cardImage() {
+		let name = cardsDirectory + this._value;
+		switch (this._suit) {
 			case 1:
 				name += "_of_clubs.svg";
 				break;
@@ -24,20 +35,11 @@ class Card {
 		}
 		return name;
 	}
-
-	get value() {
-		return this._value;
-	}
-
-	get suit() {
-		return this._suit;
-	}
-
-	get fileName() {
-		return Card.cardToFilename(this._value, this._suit);
-	}
 }
 
+// Class "Deck" with methods "getCard()" and "populateCards()".
+// populateCards() fills up the deck with cards.
+// getCard() removes a card from the array and returns it.
 class Deck extends Array {
 	constructor() {
 		super();
@@ -59,11 +61,23 @@ class Deck extends Array {
 	}
 }
 
+// Class "CardStack", representing a hand of cards. Both the player and the dealer should have one instance.
+// Has a counter for aces, adjusts its total points accordingly.
+// ".getTotalPoints()" goes through the array and returns the total points of the hand, considering the aces.
+// TODO: Implement a better way to solve Aces logic. Example: 
+// get aces() {return this._aces;}
+
 class CardStack extends Array {
 	constructor() {
 		super();
 		this._totalPoints = 0;
 		this._aces = 0;
+	}
+
+	reset() {
+		this._aces = 0;
+		this._totalPoints = 0;
+		this.length = 0;
 	}
 
 	get totalPoints() {
@@ -74,6 +88,8 @@ class CardStack extends Array {
 				points += 10;
 			} else if (this[i].value === 1) {
 				aces++;
+			} else {
+				points += this[i].value;
 			}
 		}
 		while (aces--) {
@@ -87,20 +103,8 @@ class CardStack extends Array {
 	}
 }
 
+// Global variables
 const deck = new Deck;
-
-/* function populateCards(cards) {
-	for (let i = 0; i < 52; i++) {
-		let value = i % 13 + 1;
-		let suit = i % 4 + 1;
-		let card = new Card(value, suit);
-		cards.push(card);
-	}
-} */
-
-
-// Card stacks
-
 const playerCards = new CardStack;
 const dealerCards = new CardStack;
 
@@ -124,48 +128,93 @@ function dealCard(cardField, deck, faceDown = false) {
 		if (faceDown) {
 			cardImage.src = cardsDirectory + "Card_back_01.svg";
 		} else {
-			cardImage.src = cardsDirectory + card.fileName;
+			cardImage.src = card.cardImage;
 		}
 		dealerCards.push(card);
 	} else {
-		cardImage.src = cardsDirectory + card.fileName;
+		cardImage.src = card.cardImage;
 		playerCards.push(card);
 	}
 	cardImage.className = "card";
 	cardField.appendChild(cardImage);
-	gameUpdate();
 }
 
-function gameUpdate() {
-	if (playerCards.totalPoints > 21) {
-		console.log("Player bust!");
-		//count lose;
-		//restart game
-	} else if (playerCards.totalPoints === 21){
-		//disable buttons
-		//check for blackjack
-		//if no blackjack, AI starts dealing cards
-	} else if (dealerCards.totalPoints < 17){
-		dealCard(dealerCards, deck);
-	}
-}
+gameInit();
 
 // Events
 
-
-const onDealClick = function(e) {
-	e.target.disabled = true;
-	dealCard(playerCardsField, deck);
-	dealCard(dealerCardsField, deck);
+const onDealClick = function() {
+	deck.populateCards();
+	playerCards.reset();
+	dealerCards.reset();
+	playerCardsField.innerHTML = "";
+	dealerCardsField.innerHTML = "";
+	standButton.disabled = false;
+	hitButton.disabled = false;
+	dealButton.disabled = true;
 	dealCard(playerCardsField, deck);
 	dealCard(dealerCardsField, deck, true);
+	dealCard(playerCardsField, deck);
+	dealCard(dealerCardsField, deck);
+	if (playerCards.some(isTen) && playerCards.some(isAce))
+	{
+		console.log("Blackjack!");
+		gameInit();
+		return "win";
+	}
 }
 
 const onHitClick = function() {
 	dealCard(playerCardsField, deck);
+	playersTurn();
 }
-const onStandClick = function(e) {
-	
+
+// Blackjack check functions (if the player is dealt an ace and a 10/K/Q/J as the first hand, the player wins with Blackjack)
+const isTen = (card) => card.value >= 10;
+const isAce = (card) => card.value === 1;
+
+
+function playersTurn() {
+	if (playerCards.totalPoints > 21) {
+		console.log("Player bust!");
+		gameInit();
+		return "lost";
+	}
+	if (playerCards.totalPoints === 21){
+		dealersTurn();
+		return;
+	}
+}
+
+function dealersTurn() {
+			//disable buttons
+			standButton.disabled = true;
+			hitButton.disabled = true;
+			//turn card face up
+			dealerCardsField.firstChild.src = dealerCards[0].cardImage;
+			//check for blackjack
+			//if no blackjack, AI starts dealing cards
+			while (dealerCards.totalPoints < 17) {
+				dealCard(dealerCardsField, deck);
+			}
+			if (playerCards.totalPoints > dealerCards.totalPoints || dealerCards.totalPoints > 21) {
+				console.log("Player wins!");
+				gameInit();
+				return "win";
+			} else if(playerCards.totalPoints === dealerCards.totalPoints) {
+				console.log("Push");
+				gameInit();
+				return "push";
+			} else if (playerCards.totalPoints < dealerCards.totalPoints) {
+				console.log("Player lost!");
+				gameInit();
+				return "lose";
+		}
+}
+
+const onStandClick = function() {
+		dealersTurn();
+
 }
 
 dealButton.addEventListener("click", onDealClick);
@@ -173,20 +222,11 @@ hitButton.addEventListener("click", onHitClick);
 standButton.addEventListener("click", onStandClick);
 
 
-// Validator functions
-
-function getTrueValue(value, cardStack) {
-
-}
-
-// Main game loop
-
-function gameLoop() {
-	let playerPoints = 0;
-	let dealerPoints = 0;
-	for (let i = 0; i < playerCards.length; i++) {
-		playerPoints += getTrueValue(playerCards[i].value, cardStack);
-	}
+// Game initialization
+function gameInit() {
+	hitButton.disabled = true;
+	standButton.disabled = true;
+	dealButton.disabled = false;
 }
 
 // Debugging below
